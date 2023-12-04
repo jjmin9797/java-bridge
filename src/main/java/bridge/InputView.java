96224931379b5b03fc64d9641d47ee8969a8494b
gywns0417 <gywns0417@gmail.com>
2023-12-04 17:34:37 +0900

    feat: 입력 기능 구현 완료
    다리 길이 입력, 이동할 칸 입력, 게임 재시작 여부 입력 기능을 작성했습니다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,54 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static final Pattern bridgeSizePattern =  Pattern.compile("^([3-9]|1[0-9]|20)$");
+    private static final Pattern movingPattern = Pattern.compile("^[UD]$");
+    private static final Pattern gameCommandPattern = Pattern.compile("^[RQ]$");
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println("다리의 길이를 입력해주세요.");
+        String input = getUserInput();
+        if (!bridgeSizePattern.matcher(input).matches()) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return Integer.parseInt(input);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String input = getUserInput();
+        if (!movingPattern.matcher(input).matches()) {
+            throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D만 입력할 수 있습니다.");
+        }
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String input = getUserInput();
+        if (!gameCommandPattern.matcher(input).matches()) {
+            throw new IllegalArgumentException("[ERROR] 재시작 여부 입력은 R 또는 Q만 입력할 수 있습니다.");
+        }
+        return input;
+    }
+
+    private String getUserInput() {
+        return Console.readLine();
     }
 }
